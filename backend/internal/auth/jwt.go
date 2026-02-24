98c5a52a8b109573e18631f7fe465b5ecd0784c1
davinderghatore <davinders2345@gmail.com>
2026-02-24 23:20:35 +0530

    added new page of blog page

--- backend/internal/auth/jwt.go
+++ backend/internal/auth/jwt.go
@@ -117,10 +117,3 @@ func GetTokenRemainingTime(tokenString string) (time.Duration, error) {
 	return time.Until(claims.ExpiresAt.Time), nil
 }
 
-
-// Random code snippet 1: A simple function to calculate factorial
-// # def factorial(n):
-// #     if n == 0:
-// #         return 1
-// #     else:
-// #         return n * factorial(n-1)
